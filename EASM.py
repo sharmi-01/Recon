74c310f7ee4beb15d621256f61f8c9eb9de7413b
Nagasharmila P <107355774+sharmi-01@users.noreply.github.com>
2024-01-22 17:12:08 +0530

    Update EASM.py

--- EASM.py
+++ EASM.py
@@ -1,7 +1,7 @@
 import os
 domain=input("Give the Domain:")
 url = f"https://{domain}"
-#runs final recon
+#runs recon1.py
 os.system(f"python3 recon1.py --headers --sslinfo --whois --crawl --dns --sub --wayback --ps -r -s {url}")
-#runs Sws-recon
+#runs recon2.py
 os.system(f"python3 recon2.py -d {domain} -o -s -jl -t -w -r --email")
